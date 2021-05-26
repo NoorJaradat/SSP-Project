22aec011292fa88b7e3c896f3457bb9a622ff602
NoorJaradat <50170857+NoorJaradat@users.noreply.github.com>
2021-05-26 16:58:58 +0300

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Assignment3.Models;
 using Assignment3.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +13,18 @@ namespace Assignment3.Controllers
     public class HomeController : Controller
     {
         private INurseRepository repository;
+        private IWebHostEnvironment iHost;
 
-        public HomeController(/*INurseRepository nurseRepository*/)
+        public HomeController(IWebHostEnvironment iHost, INurseRepository nurseRepository)
         {
-           // repository = nurseRepository;
-           repository = new NurseRepository();
+           repository = nurseRepository;
+           //repository = new NurseRepository();
+            this.iHost = iHost;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Nurse> nurses = repository.getAll();
+            IEnumerable<Nurse> nurses = repository.ShowAllNurses();
             return View(nurses);
         }
         public ViewResult NurseDetails(int id)
@@ -40,11 +44,29 @@ namespace Assignment3.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create(Nurse nurse)
+        public ActionResult Create(NurseCreateViewModel model)
         {
             if(ModelState.IsValid)
             {
-                Nurse newNurse=repository.Add(nurse);
+                string uniqueFileName = null;
+                if(model.Photo!=null)
+                {
+                    string uploadFolder=Path.Combine(iHost.WebRootPath,"img");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadFolder, uniqueFileName);
+                    FileStream fileStream = new FileStream(filePath, FileMode.Create);
+                    model.Photo.CopyTo(fileStream);
+                }
+                Nurse newNurse = new Nurse()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Section = model.Section,
+                    PhotoName = uniqueFileName
+                };
+                repository.Add(newNurse);
+
+                
                 return RedirectToAction("NurseDetails", new { newNurse.Id });
             }
             return View();
